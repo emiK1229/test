dc9383cb8e390dba078d45008483ade84934677f
emiK1229 <minp1229+gh@gmail.com>
2019-04-25 17:53:37 +0900

    2019/04/25

--- login/src/com/internousdev/login/util/DBConnector.java
+++ login/src/com/internousdev/login/util/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnector {
 
 	private static String driverName ="com.mysql.jdbc.Driver";
-	private static String url="jdbc:mysql://localhost/logindb_kitamura";
+	private static String url="jdbc:mysql://localhost/logindb3_kitamura";
 
 	private static String user="root";
 	private static String password="mysql";
